80dcab8ffc674cb8e7aec165fdb0c5cc0c118582
multiahfoon <multiahfoon@gmail.com>
2022-08-05 21:03:41 +1200

    wip: import database and allow client side env

--- firebase.ts
+++ firebase.ts
@@ -1,19 +1,22 @@
 import { getApp, getApps, initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
+import { getAuth } from 'firebase/auth'
+import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 // Check if app is already initialized else get current instance of app
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-const db = getFirestore()
-const storage = getStorage()
+const storage = getStorage(app)
+const auth = getAuth(app)
+const db = getDatabase(app)
 
-export { app, db, storage }
+export { auth, db, storage }
