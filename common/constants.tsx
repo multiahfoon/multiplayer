bb9a1007e9076729db38efe760fc749b7e939929
multiahfoon <multiahfoon@gmail.com>
2022-08-06 10:57:47 +1200

    wip: add types

--- common/constants.tsx
+++ common/constants.tsx
@@ -1,4 +1,21 @@
-export const ANIMAL_LIST = [
+import { ArrowKeys, Key, tBlockSpaces } from '../types'
+
+const blockedSpaces: tBlockSpaces = {
+  '7x4': true,
+  '1x11': true,
+  '12x10': true,
+  '4x7': true,
+  '5x7': true,
+  '6x7': true,
+  '8x6': true,
+  '9x6': true,
+  '10x6': true,
+  '7x9': true,
+  '8x9': true,
+  '9x9': true,
+}
+
+export const ANIMAL_LIST: string[] = [
   'BEAR',
   'BIRD',
   'BOAR',
@@ -16,7 +33,7 @@ export const ANIMAL_LIST = [
   'VOLE',
 ]
 
-export const ARROW_KEYS = {
+export const ARROW_KEYS: ArrowKeys = {
   ArrowUp: { x: 0, y: -1 },
   ArrowDown: { x: 0, y: 1 },
   ArrowLeft: { x: -1, y: 0 },
@@ -28,23 +45,36 @@ export const MAP_DATA = {
   maxX: 14,
   minY: 4,
   maxY: 12,
-  blockedSpaces: {
-    '7x4': true,
-    '1x11': true,
-    '12x10': true,
-    '4x7': true,
-    '5x7': true,
-    '6x7': true,
-    '8x6': true,
-    '9x6': true,
-    '10x6': true,
-    '7x9': true,
-    '8x9': true,
-    '9x9': true,
-  },
+  blockedSpaces,
 }
 
-export const PREFIX = [
+export const RANDOM_POSITION: Key[] = [
+  { x: 1, y: 4 },
+  { x: 2, y: 4 },
+  { x: 1, y: 5 },
+  { x: 2, y: 6 },
+  { x: 2, y: 8 },
+  { x: 2, y: 9 },
+  { x: 4, y: 8 },
+  { x: 5, y: 5 },
+  { x: 5, y: 8 },
+  { x: 5, y: 10 },
+  { x: 5, y: 11 },
+  { x: 11, y: 7 },
+  { x: 12, y: 7 },
+  { x: 13, y: 7 },
+  { x: 13, y: 6 },
+  { x: 13, y: 8 },
+  { x: 7, y: 6 },
+  { x: 7, y: 7 },
+  { x: 7, y: 8 },
+  { x: 8, y: 8 },
+  { x: 10, y: 8 },
+  { x: 8, y: 8 },
+  { x: 11, y: 4 },
+]
+
+export const PREFIX: string[] = [
   'BUFF',
   'COOL',
   'COOL',
@@ -64,7 +94,7 @@ export const PREFIX = [
   'WARM',
 ]
 
-export const playerColors = [
+export const PLAYER_COLORS: string[] = [
   'blue',
   'red',
   'orange',
