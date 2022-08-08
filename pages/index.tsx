ec8bc9516388d4b1c07142cb3078a78aef5c7930
multiahfoon <multiahfoon@gmail.com>
2022-08-08 21:35:40 +1200

    refactor: import play and coin components
    player and coin logic have been moved into their related components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect, useRef, useState } from 'react'
+import type { NextPage } from 'next'
+
 import { onAuthStateChanged, signInAnonymously } from 'firebase/auth'
-import {
-  onChildAdded,
-  onChildRemoved,
-  onDisconnect,
-  onValue,
-  ref,
-  remove,
-  set,
-  update,
-} from 'firebase/database'
+import { onDisconnect, ref, remove, set, update } from 'firebase/database'
+import { useEffect, useRef, useState } from 'react'
+import { useRecoilState } from 'recoil'
+
+import { auth, database } from '../firebase'
 
 import {
   createName,
@@ -21,92 +16,75 @@ import {
   randomFromArray,
 } from '../lib/miscHandlers'
 
-import { auth, database } from '../firebase'
-
 import { ARROW_KEYS, PLAYER_COLORS } from '../common/constants'
 import { playerIdState } from '../atoms/playerIdAtom'
-import { useRecoilState } from 'recoil'
+import { playerNameState } from '../atoms/playerNameAtom'
+import { playersState } from '../atoms/playersAtom'
+
+import { Coins } from '../components/Coins'
+import { PlayerColorBtn } from '../components/PlayerColorBtn'
+import { PlayerNameInput } from '../components/PlayerNameInput'
+import { Players } from '../components/Players'
 
 const Home: NextPage = () => {
+  const [, setPlayerName] = useRecoilState<any>(playerNameState)
   const [keyPressed, setKeyPressed] = useState<boolean>(true)
-
-  const coinElements = useRef<any>({})
   const coins = useRef<any>({})
-  const gameContainer = useRef<any>() // .game-container
-  const playerElements = useRef<any>({})
-  const playerNameInput = useRef<any>() // #player-name
-  const players = useRef<any>({})
-  const [playerId, setPlayerId] = useRecoilState(playerIdState)
+  const [playerId, setPlayerId] = useRecoilState<any>(playerIdState)
+  const [players, setPlayers] = useRecoilState<any>(playersState)
 
   // onMount sign in as anonymous user
   useEffect(() => {
-    handleAuth()
+    if (!playerId) handleAuth()
   }, [])
-
-  useEffect(() => {
-    if (playerId) initGame()
-  }, [playerId])
 
   // key press event listeners
   useEffect(() => {
-    window.addEventListener('keydown', keydownFunction)
-    window.addEventListener('keyup', keyupFunction)
+    if (!playerId || !players) return
+    window.addEventListener('keydown', keyDown)
+    window.addEventListener('keyup', keyUp)
 
     // Remove event listeners on cleanup
     return () => {
-      window.removeEventListener('keydown', keydownFunction)
-      window.removeEventListener('keyup', keyupFunction)
+      window.removeEventListener('keydown', keyDown)
+      window.removeEventListener('keyup', keyUp)
     }
-  }, [])
+  }, [playerId, players])
 
-  async function handleColorBtnClick() {
-    const playerRef = ref(database, `/players/${playerId}`)
-
-    const mySkinIndex = PLAYER_COLORS.indexOf(players.current[playerId].color)
-
-    const nextColor = PLAYER_COLORS[mySkinIndex + 1] || PLAYER_COLORS[0]
-
-    await update(playerRef, {
-      color: nextColor,
-    })
-  }
-
-  // If pressed key is our target key then set to true
-  function keydownFunction({ key }: { key: string }) {
+  async function keyDown({ key }: { key: string }) {
     if (ARROW_KEYS.hasOwnProperty(key) && keyPressed) {
-      // update position
-      ARROW_KEYS
-      keyArrowPress(ARROW_KEYS[key].x, ARROW_KEYS[key].y)
+      await arrowKeyPress(ARROW_KEYS[key].x, ARROW_KEYS[key].y)
+
       setKeyPressed(false)
     }
   }
 
-  // If released key is our target key then set to false
-  const keyupFunction = ({ key }: { key: string }) => {
+  function keyUp({ key }: { key: string }) {
     if (ARROW_KEYS[key]) {
       setKeyPressed(true)
     }
   }
 
-  function keyArrowPress(xChange = 0, yChange = 0) {
-    const playerRef = ref(database, `/players/${playerId}`)
+  async function arrowKeyPress(xChange = 0, yChange = 0) {
+    const newX = Number(players[playerId].x) + xChange
+    const newY = Number(players[playerId].y) + yChange
 
-    const newX = players.current[playerId].x + xChange
-    const newY = players.current[playerId].y + yChange
     if (!isSolid(newX, newY)) {
-      //move to the next space
-      players.current[playerId].x = newX
-      players.current[playerId].y = newY
-      if (xChange === 1) {
-        players.current[playerId].direction = 'right'
-      }
-      if (xChange === -1) {
-        players.current[playerId].direction = 'left'
+      const newPosition = {
+        ...players[playerId],
+        x: newX,
+        y: newY,
+        direction: xChange === 1 ? 'right' : 'left',
       }
 
-      set(playerRef, players.current[playerId])
+      await set(ref(database, `/players/${playerId}`), newPosition)
 
-      attemptGrabCoin(newX, newY)
+      setPlayers({
+        ...players,
+        [playerId]: newPosition,
+      })
+
+      await attemptGrabCoin(newX, newY)
     }
   }
 
@@ -115,16 +93,15 @@ const Home: NextPage = () => {
 
     onAuthStateChanged(auth, async (user) => {
       if (user) {
-        //You're logged in!
+        // user successfully logged in
         const id = user.uid
-        setPlayerId(id)
-
         const name = createName()
-        playerNameInput.current = name
-
         const playerRef = ref(database, `/players/${id}`)
 
         const { x, y } = getRandomSafeSpot()
+
+        setPlayerId(id)
+        setPlayerName(name)
 
         await set(playerRef, {
           id,
@@ -136,22 +113,9 @@ const Home: NextPage = () => {
           coins: 0,
         })
 
-        //Remove me from Firebase when I disconnect
+        //Remove current player when they leave the game
         await onDisconnect(playerRef).remove()
-
-        // Begin the game now that we are signed in
       }
-    })
-  }
-
-  async function handleNameChange(e: any) {
-    const playerRef = ref(database, `/players/${playerId}`)
-
-    const newName = e.target.value || createName()
-    playerNameInput.current = newName
-
-    await update(playerRef, {
-      name: newName,
     })
   }
 
@@ -165,125 +129,9 @@ const Home: NextPage = () => {
       await remove(ref(database, `coins/${key}`))
 
       await update(playerRef, {
-        coins: players.current[playerId].coins + 1,
+        coins: players[playerId].coins + 1,
       })
     }
-  }
-
-  async function placeCoin() {
-    const { x, y } = getRandomSafeSpot()
-    const coinRef = ref(database, `coins/${getKeyString(x, y)}`)
-
-    await set(coinRef, { x, y })
-
-    const coinTimeouts = [2000, 3000, 4000, 5000]
-
-    setTimeout(() => {
-      placeCoin()
-    }, randomFromArray(coinTimeouts))
-  }
-
-  async function initGame() {
-    const allPlayersRef = ref(database, '/players')
-    const allCoinsRef = ref(database, '/coins')
-
-    onChildAdded(allPlayersRef, (snapshot) => {
-      //Fires whenever a new node is added the tree
-      const addedPlayer = snapshot.val()
-
-      const characterElement = document.createElement('div')
-      characterElement.classList.add('Character', 'grid-cell')
-
-      if (addedPlayer.id === playerId) {
-        characterElement.classList.add('you')
-      }
-
-      characterElement.innerHTML = `
-          <div class="Character_shadow grid-cell"></div>
-          <div class="Character_sprite grid-cell"></div>
-          <div class="Character_name-container">
-            <span class="Character_name">${addedPlayer.name}</span>
-            <span class="Character_coins">${addedPlayer.coins}</span>
-          </div>
-          <div class="Character_you-arrow"></div>
-        `
-
-      playerElements.current[addedPlayer.id] = characterElement
-
-      //Fill in some initial state
-      characterElement.setAttribute('data-color', addedPlayer.color)
-      characterElement.setAttribute('data-direction', addedPlayer.direction)
-      const left = 16 * addedPlayer.x + 'px'
-      const top = 16 * addedPlayer.y - 4 + 'px'
-      characterElement.style.transform = `translate3d(${left}, ${top}, 0)`
-
-      gameContainer.current.appendChild(characterElement)
-    })
-
-    onValue(allPlayersRef, (snapshot) => {
-      //Fires whenever a change occurs
-      players.current = snapshot.val() || {}
-      Object.keys(players.current).forEach((key) => {
-        const characterState = players.current[key]
-        let el = playerElements.current[key]
-
-        // Now update the DOM
-        el.querySelector('.Character_name').innerText = characterState.name
-        el.querySelector('.Character_coins').innerText = characterState.coins
-
-        el.setAttribute('data-color', characterState.color)
-        el.setAttribute('data-direction', characterState.direction)
-        const left = 16 * characterState.x + 'px'
-        const top = 16 * characterState.y - 4 + 'px'
-        el.style.transform = `translate3d(${left}, ${top}, 0)`
-      })
-    })
-
-    // Remove character DOM element after they leave
-    onChildRemoved(allPlayersRef, (snapshot) => {
-      const removedKey = snapshot.val().id
-
-      gameContainer.current.removeChild(playerElements.current[removedKey])
-      delete playerElements.current[removedKey]
-    })
-
-    // This block will remove coins.current from local state when Firebase `coins.current` value updates
-    onValue(allCoinsRef, (snapshot) => {
-      coins.current = snapshot.val() || {}
-    })
-
-    onChildAdded(allCoinsRef, (snapshot) => {
-      const coin = snapshot.val()
-      const key = getKeyString(coin.x, coin.y)
-      coins.current[key] = true
-
-      // Create the DOM Element
-      const coinElement = document.createElement('div')
-      coinElement.classList.add('Coin', 'grid-cell')
-      coinElement.innerHTML = `
-        <div class="Coin_shadow grid-cell"></div>
-        <div class="Coin_sprite grid-cell"></div>
-      `
-
-      // Position the Element
-      const left = 16 * coin.x + 'px'
-      const top = 16 * coin.y - 4 + 'px'
-      coinElement.style.transform = `translate3d(${left}, ${top}, 0)`
-
-      // Keep a reference for removal later and add to DOM
-      coinElements.current[key] = coinElement
-      gameContainer.current.appendChild(coinElement)
-    })
-
-    onChildRemoved(allCoinsRef, (snapshot) => {
-      const { x, y } = snapshot.val()
-      const keyToRemove = getKeyString(x, y)
-
-      gameContainer.current.removeChild(coinElements.current[keyToRemove])
-      delete coinElements.current[keyToRemove]
-    })
-
-    placeCoin()
   }
 
   return (
@@ -294,21 +142,13 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <div ref={gameContainer} className='game-container' />
-      <div className='player-info'>
-        <div>
-          <label>Your Name</label>
-          <input
-            value={playerNameInput.current}
-            onChange={handleNameChange}
-            maxLength={10}
-            type='text'
-          />
-        </div>
+      <div className='game-container'>
+        <Players />
+      </div>
 
-        <div>
-          <button onClick={handleColorBtnClick}>Change Color</button>
-        </div>
+      <div className='player-info'>
+        <PlayerNameInput />
+        <PlayerColorBtn />
       </div>
     </div>
   )
