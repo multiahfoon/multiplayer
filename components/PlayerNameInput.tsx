b5425f6367f84488d1b0c6233454eea8283c740b
multiahfoon <multiahfoon@gmail.com>
2022-08-08 16:53:06 +1200

    wip: add player name input component

---
+++ components/PlayerNameInput.tsx
@@ -0,0 +1,32 @@
+import { ref, update } from 'firebase/database'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { playerIdState } from '../atoms/playerIdAtom'
+import { playerNameState } from '../atoms/playerNameAtom'
+import { database } from '../firebase'
+
+export function PlayerNameInput() {
+  const playerId = useRecoilValue<string>(playerIdState)
+  const [playerName, setPlayerName] = useRecoilState<string>(playerNameState)
+
+  async function handleNameChange(e: any) {
+    const newName = e.target.value
+    const playerRef = ref(database, `/players/${playerId}`)
+    setPlayerName(newName)
+
+    await update(playerRef, {
+      name: newName,
+    })
+  }
+
+  return (
+    <div>
+      <label>Your Name</label>
+      <input
+        value={playerName}
+        onChange={handleNameChange}
+        maxLength={10}
+        type='text'
+      />
+    </div>
+  )
+}
