db82232fa694c1a5a5a17a25d36b0d50d6877f5b
multiahfoon <multiahfoon@gmail.com>
2022-08-08 19:35:38 +1200

    wip: declare props as player type

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,13 +3,16 @@ import React from 'react'
 import { useRecoilValue } from 'recoil'
 
 import { playerIdState } from '../atoms/playerIdAtom'
+import { Player } from '../types'
 
-export function Player({ coins, color, direction, id, name, x, y }: any) {
+export function Player({ coins, color, direction, id, name, x, y }: Player) {
   const playerId = useRecoilValue(playerIdState)
+  // @TODO fix: id is read as a number but only in player component
+  const uuid = `${id}`
 
   return (
     <div
-      className={`Character grid-cell ${id === playerId && 'you'} 
+      className={`Character grid-cell ${uuid === playerId ? 'you' : ''}
           data-color=${color}
           data-direction=${direction}
           `}
@@ -18,8 +21,8 @@ export function Player({ coins, color, direction, id, name, x, y }: any) {
       <div className='Character_shadow grid-cell' />
       <div className='Character_sprite grid-cell' />
       <div className='Character_name-container'>
-        <span className='Character_name'>${name}</span>
-        <span className='Character_coins'>${coins}</span>
+        <span className='Character_name'>{name}</span>
+        <span className='Character_coins'>{coins}</span>
       </div>
       <div className='Character_you-arrow' />
     </div>
