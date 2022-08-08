e82b5373b752803eef4c70b5fe103f02f3f15b3e
multiahfoon <multiahfoon@gmail.com>
2022-08-08 16:52:43 +1200

    wip: add player color button component

---
+++ components/PlayerColorBtn.tsx
@@ -0,0 +1,31 @@
+import { ref, update } from 'firebase/database'
+import { useRef } from 'react'
+import { useRecoilValue } from 'recoil'
+import { playerIdState } from '../atoms/playerIdAtom'
+import { playersState } from '../atoms/playersAtom'
+import { PLAYER_COLORS } from '../common/constants'
+import { database } from '../firebase'
+
+export function PlayerColorBtn() {
+  const playerId = useRecoilValue<any>(playerIdState)
+  const playerName = useRef<string>('')
+  const players = useRecoilValue<any>(playersState)
+
+  async function handleColorBtnClick() {
+    const playerRef = ref(database, `/players/${playerId}`)
+
+    const mySkinIndex = PLAYER_COLORS.indexOf(players[playerId].color)
+
+    const nextColor = PLAYER_COLORS[mySkinIndex + 1] || PLAYER_COLORS[0]
+
+    await update(playerRef, {
+      color: nextColor,
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={handleColorBtnClick}>Change Color</button>
+    </div>
+  )
+}
